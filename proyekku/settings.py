81a1c6990137e59045ca180be425e2b7e6635c23
Moh Ariful <nextkoding@gmail.com>
2023-01-08 19:39:34 +0700

    Update settings.py

--- proyekku/settings.py
+++ proyekku/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!i1x_*fzzchs9vwr!ripv&m1spo#)%ev!*2kpse2qxx&z7(d5%'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
